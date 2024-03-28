64fa90d9a1443de17fb71c239cbef2bf77dc3b23
bmazou <codprojmail@gmail.com>
2024-03-28 21:02:29 +0100

    Add lambda parameter to fit method. Add general recommendations

--- scripts/recommender.py
+++ scripts/recommender.py
@@ -7,18 +7,17 @@ from scripts.utils import Utils
 class Recommender:
     def __init__(self, ratings_path, movie_names_path):
         self.ease = EASE()
-        self.ratings = self.read_ratings_to_df(ratings_path)
         self.movie_names = self.read_movies_names_to_df(movie_names_path)
-        
+        self.ratings = self.read_ratings_to_df(ratings_path)
+
     def read_ratings_to_df(self, file_path):
         df = pd.read_json(file_path, lines=True)
         return df[['user_id', 'item_id', 'rating']]
-        
+
     def read_movies_names_to_df(self, file_path):
         df = pd.read_json(file_path, lines=True)
         return df[['item_id', 'title', 'imdbId']]
-
-
+    
     @staticmethod
     def get_movie_id(movies, movie_name):
         best_distance = float('inf')
@@ -35,18 +34,24 @@ class Recommender:
         print(f'Looking for "{movie_name}". Found "{found_movie_name}"')
         return found_movie_id
 
-    def fit(self, model_path=None):
-        self.ease.fit(self.ratings, implicit=False, model_path=model_path)
+    def fit(self, model_path=None, lambda_=0.5):
+        self.ease.fit(self.ratings, implicit=False, model_path=model_path, lambda_=lambda_)
 
     def merge_with_movie_names(self, predictions):
         return predictions.merge(self.movie_names, on='item_id').sort_values('score', ascending=False)
 
     def predict_most_popular(self, k=10):
-        return self.ratings.groupby('item_id').size().sort_values(ascending=False).head(k).reset_index(name='score')
+        movie_counts = self.ratings['item_id'].value_counts()
+        most_popular = movie_counts[:k].index
+        return self.merge_with_movie_names(pd.DataFrame({'item_id': most_popular, 'score': movie_counts[:k].values}))
 
 
-    def predict(self, user_ratings, k=10):
-        user_ratings = [(self.get_movie_id(self.movie_names, movie), rating) for movie, rating in user_ratings]
+    def predict(self, user_ratings, k=10, inputs_are_ids=False):
+        no_ratings_provided = len(user_ratings) == 0
+        if no_ratings_provided:
+            return self.predict_most_popular(k)
+        
+        user_ratings = user_ratings if inputs_are_ids else [(self.get_movie_id(self.movie_names, movie), rating) for movie, rating in user_ratings]
 
         predictions =  self.ease.predict(user_ratings, k)
         return self.merge_with_movie_names(predictions)
